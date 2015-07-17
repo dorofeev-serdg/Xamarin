331203ad5229db53dfdca96b5c5dc9d14f8a19da
Sergey Dorofeev <sdorofeev@ruswizards.com>
2015-07-17 09:20:08 +0300

    iOS part (portret) is added.

---
+++ Calculator/iOS/Controllers/CalcViewController.cs
@@ -0,0 +1,121 @@
+﻿
+using System;
+
+using Foundation;
+using UIKit;
+using Core;
+
+namespace iOS
+{
+	public partial class CalcViewController : UIViewController
+	{
+		Calc calc;
+
+		public CalcViewController () : base ("CalcViewController", null)
+		{
+			calc = new Calc ();
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			// Releases the view if it doesn't have a superview.
+			base.DidReceiveMemoryWarning ();
+			
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			
+			// Perform any additional setup after loading the view, typically from a nib.
+
+			Number0.TouchUpInside += (object sender, EventArgs e) => { NumberX_Click(0); };
+			Number1.TouchUpInside += (object sender, EventArgs e) => { NumberX_Click(1); };
+			Number2.TouchUpInside += (object sender, EventArgs e) => { NumberX_Click(2); };
+			Number3.TouchUpInside += (object sender, EventArgs e) => { NumberX_Click(3); };
+			Number4.TouchUpInside += (object sender, EventArgs e) => { NumberX_Click(4); };
+			Number5.TouchUpInside += (object sender, EventArgs e) => { NumberX_Click(5); };
+			Number6.TouchUpInside += (object sender, EventArgs e) => { NumberX_Click(6); };
+			Number7.TouchUpInside += (object sender, EventArgs e) => { NumberX_Click(7); };
+			Number8.TouchUpInside += (object sender, EventArgs e) => { NumberX_Click(8); };
+			Number9.TouchUpInside += (object sender, EventArgs e) => { NumberX_Click(9); };
+
+
+			ActionPlus.TouchUpInside   += (object sender, EventArgs e) => { ActionX_Click(Calc.Actions.Plus);};
+			ActionMinus.TouchUpInside  += (object sender, EventArgs e) => { ActionX_Click(Calc.Actions.Minus);};
+			ActionMul.TouchUpInside    += (object sender, EventArgs e) => { ActionX_Click(Calc.Actions.Mul);};
+			ActionDiv.TouchUpInside    += (object sender, EventArgs e) => { ActionX_Click(Calc.Actions.Div);};
+			ActionResult.TouchUpInside += (object sender, EventArgs e) => { Result_Click();};
+
+			ActionClear.TouchUpInside += (object sender, EventArgs e) => {
+				PressClear ();
+			};
+
+			ActionBackspace.TouchUpInside += (object sender, EventArgs e) => {
+				PressBackspace ();
+			};
+
+			Dot.TouchUpInside += (object sender, EventArgs e) => {
+				PressDot();
+			};
+		}
+
+		private void NumberX_Click (int number)
+		{
+			calc.PressNumber(number);
+			Console.WriteLine (String.Format("Press number {0}", number));
+			Digits.Text = calc.Argument.ToString("F" + ( calc.Dot < 0 ? "0" : calc.Dot.ToString()));
+		}
+
+		private void ActionX_Click(Calc.Actions action)
+		{
+			try
+			{
+				calc.PressAction (action);
+			}
+			catch {
+				Digits.Text = "Error!";
+				calc.PressClear ();
+				return;
+			}
+			Console.WriteLine (String.Format("Press action {0}", action));
+			Digits.Text = calc.Result.ToString ();
+		}
+
+		private void Result_Click()
+		{
+			calc.PressResult ();
+			Console.WriteLine ("Press result");
+			try
+			{
+				Digits.Text = calc.Result.ToString ();
+			}
+			catch {
+				Digits.Text = "Error!";
+				calc.PressClear ();
+			}
+		}
+
+		private void PressDot()
+		{
+			calc.PressDot ();
+			Console.WriteLine ("Press dot");
+		}
+
+		private void PressClear()
+		{
+			calc.PressClear ();
+			Console.WriteLine ("Press clear");
+			Digits.Text = calc.Argument.ToString ();
+		}
+
+		private void PressBackspace()
+		{
+			calc.PressBackspace ();
+			Console.WriteLine ("Press backspace");
+			Digits.Text = calc.Argument.ToString ("F" + ( calc.Dot < 0 ? "0" : calc.Dot.ToString()));
+		}
+	}
+}
+
---
+++ Calculator/iOS/Controllers/CalcViewController.designer.cs
@@ -0,0 +1,170 @@
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace iOS
+{
+	[Register ("CalcViewController")]
+	partial class CalcViewController
+	{
+		[Outlet]
+		UIKit.UIButton ActionBackspace { get; set; }
+
+		[Outlet]
+		UIKit.UIButton ActionClear { get; set; }
+
+		[Outlet]
+		UIKit.UIButton ActionDiv { get; set; }
+
+		[Outlet]
+		UIKit.UIButton ActionMinus { get; set; }
+
+		[Outlet]
+		UIKit.UIButton ActionMul { get; set; }
+
+		[Outlet]
+		UIKit.UIButton ActionPlus { get; set; }
+
+		[Outlet]
+		UIKit.UIButton ActionResult { get; set; }
+
+		[Outlet]
+		UIKit.UILabel Digits { get; set; }
+
+		[Outlet]
+		UIKit.UIButton Dot { get; set; }
+
+		[Outlet]
+		UIKit.UIButton Number0 { get; set; }
+
+		[Outlet]
+		UIKit.UIButton Number1 { get; set; }
+
+		[Outlet]
+		UIKit.UIButton Number2 { get; set; }
+
+		[Outlet]
+		UIKit.UIButton Number3 { get; set; }
+
+		[Outlet]
+		UIKit.UIButton Number4 { get; set; }
+
+		[Outlet]
+		UIKit.UIButton Number5 { get; set; }
+
+		[Outlet]
+		UIKit.UIButton Number6 { get; set; }
+
+		[Outlet]
+		UIKit.UIButton Number7 { get; set; }
+
+		[Outlet]
+		UIKit.UIButton Number8 { get; set; }
+
+		[Outlet]
+		UIKit.UIButton Number9 { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (ActionBackspace != null) {
+				ActionBackspace.Dispose ();
+				ActionBackspace = null;
+			}
+
+			if (ActionClear != null) {
+				ActionClear.Dispose ();
+				ActionClear = null;
+			}
+
+			if (ActionDiv != null) {
+				ActionDiv.Dispose ();
+				ActionDiv = null;
+			}
+
+			if (ActionMinus != null) {
+				ActionMinus.Dispose ();
+				ActionMinus = null;
+			}
+
+			if (ActionMul != null) {
+				ActionMul.Dispose ();
+				ActionMul = null;
+			}
+
+			if (ActionPlus != null) {
+				ActionPlus.Dispose ();
+				ActionPlus = null;
+			}
+
+			if (ActionResult != null) {
+				ActionResult.Dispose ();
+				ActionResult = null;
+			}
+
+			if (Digits != null) {
+				Digits.Dispose ();
+				Digits = null;
+			}
+
+			if (Dot != null) {
+				Dot.Dispose ();
+				Dot = null;
+			}
+
+			if (Number0 != null) {
+				Number0.Dispose ();
+				Number0 = null;
+			}
+
+			if (Number1 != null) {
+				Number1.Dispose ();
+				Number1 = null;
+			}
+
+			if (Number2 != null) {
+				Number2.Dispose ();
+				Number2 = null;
+			}
+
+			if (Number3 != null) {
+				Number3.Dispose ();
+				Number3 = null;
+			}
+
+			if (Number4 != null) {
+				Number4.Dispose ();
+				Number4 = null;
+			}
+
+			if (Number5 != null) {
+				Number5.Dispose ();
+				Number5 = null;
+			}
+
+			if (Number6 != null) {
+				Number6.Dispose ();
+				Number6 = null;
+			}
+
+			if (Number7 != null) {
+				Number7.Dispose ();
+				Number7 = null;
+			}
+
+			if (Number8 != null) {
+				Number8.Dispose ();
+				Number8 = null;
+			}
+
+			if (Number9 != null) {
+				Number9.Dispose ();
+				Number9 = null;
+			}
+		}
+	}
+}
